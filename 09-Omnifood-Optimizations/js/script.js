7f8481c30a2b136edac5618592a89b386de59012
Shweta Tyagi <tyagishweta341@gmail.com>
2022-05-10 15:50:57 +0200

    intial 09

---
+++ 09-Omnifood-Optimizations/js/script.js
@@ -0,0 +1,108 @@
+"use strict";
+// current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+////////////////////////////////////////////////
+// making mob nav work
+////////////////////////////////////////////////
+
+const headerEl = document.querySelector(".header");
+const btnMobNavEl = document.querySelector(".btn-mobile-nav");
+
+btnMobNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////
+// better method at line 53
+
+//const headerNavLinkEl = document.querySelectorAll(".header-nav-link");
+// headerNavLinkEl.forEach(function (navLink) {
+//   navLink.addEventListener("click", function () {
+//     headerEl.classList.remove("nav-open");
+//   });
+// });
+
+////////////////////////////////////////////////
+// smooth scroll behaviour
+////////////////////////////////////////////////
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    //scroll to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // scroll to other links
+    if (href != "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //close mobile nav
+    if (link.classList.contains("header-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+/////////////////////////////////////////////
+// making nav sticky
+//////////////////////////////////////////////
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const entry = entries[0];
+    console.log(entry);
+
+    if (entry.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (entry.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+
+  {
+    //in the view port
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+observer.observe(sectionHeroEl);
+
+/////////////////////////////////////////////////
+//// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
